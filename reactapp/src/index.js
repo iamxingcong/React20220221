269898edd7ed00b2d282f7d45179534bc01b088d
iamxingcong <121950263@qq.com>
2022-02-21 18:47:08 +0800

    update

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import FrontPage from "./views/FrontPage";
+
+
+import store from './app/store'
+import { Provider } from 'react-redux'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="FrontPage" element={<FrontPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ reactapp/src/views/FrontPage.js
@@ -0,0 +1,17 @@
+
+
+import React from "react";
+class FrontPage extends React.Component {
+
+	render() {
+      
+        return (
+            <div id="createevent">
+                sfd
+            </div>
+
+            )
+        }
+
+}
+export default FrontPage
